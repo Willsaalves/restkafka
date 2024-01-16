315e31c047a5e93ec52cb01c30ded64a2315d4b0
Willsaalves <will.saalves@gmail.com>
2024-01-16 15:52:33 -0300

    feat: principal class

---
+++ src/main/java/dio/expertostech/rest/kafka/RestKafkaApplication.java
@@ -0,0 +1,16 @@
+package dio.expertostech.rest.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+
+
+@EnableKafka
+@SpringBootApplication
+public class RestKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dio/expertostech/restkafka/RestKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.expertostech.restkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
